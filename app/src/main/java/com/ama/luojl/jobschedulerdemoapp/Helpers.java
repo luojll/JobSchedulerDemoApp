c5036836eaaf90fd0610a149a503d0ee5aa8cc47
Jun Luo <luojl@amazon.com>
2017-07-25 19:19:15 +0800

    Add log "scheduled" in Helpers.java

--- app/src/main/java/com/ama/luojl/jobschedulerdemoapp/Helpers.java
+++ app/src/main/java/com/ama/luojl/jobschedulerdemoapp/Helpers.java
@@ -4,12 +4,14 @@ import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
+import android.util.Log;
 
 /**
  * Created by luojl on 7/25/17.
  */
 
 public class Helpers {
+    private static final String TAG = "Helpers";
 
     public static void scheduleJob(Context context, int id) {
         final JobScheduler scheduler = getJobScheduler(context);
@@ -20,6 +22,7 @@ public class Helpers {
         builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED);
 
         scheduler.schedule(builder.build());
+        Log.v(TAG, "id: " + id + ", scheduled");
     }
 
     public static JobScheduler getJobScheduler(Context context) {
